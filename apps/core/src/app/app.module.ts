489dfc43d4a6976ee58e91f95a1d24b8aa512466
Edmond Ó Floinn <oflynned@tcd.ie>
2021-04-29 18:23:33 +0200

    add initial type definitions

--- apps/core/src/app/app.module.ts
+++ apps/core/src/app/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {join} from 'path'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), './types/generated-types.ts'),
+        outputAs: 'interface'
+      },
+      debug: true,
+      playground: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ types/generated-types.ts
@@ -0,0 +1,201 @@
+
+/*
+ * ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export enum Frequency {
+    DAILY = "DAILY",
+    WEEKLY = "WEEKLY",
+    FORTNIGHTLY = "FORTNIGHTLY",
+    MONTHLY = "MONTHLY"
+}
+
+export enum QuestionType {
+    RADIO = "RADIO",
+    CHECKBOX = "CHECKBOX",
+    SCALE = "SCALE",
+    TEXT = "TEXT"
+}
+
+export interface ResearcherInput {
+    name: string;
+    dateOfBirth: string;
+    email: string;
+    username: string;
+}
+
+export interface CandidateInput {
+    name: string;
+    dateOfBirth: string;
+    email: string;
+    username: string;
+}
+
+export interface PageInfo {
+    hasPreviousPage: boolean;
+    hasNextPage: boolean;
+    startCursor?: Cursor;
+    endCursor?: Cursor;
+}
+
+export interface Audit {
+    id: string;
+    action: string;
+    performedAt: DateTime;
+    performedBy: Actor;
+}
+
+export interface AuditEdge {
+    node?: Audit;
+    cursor: Cursor;
+}
+
+export interface AuditConnection {
+    totalCount: number;
+    pageInfo: PageInfo;
+    edges: AuditEdge[];
+}
+
+export interface Candidate {
+    id: string;
+    createdAt: DateTime;
+    deletedAt?: DateTime;
+    verifiedAt?: DateTime;
+    auditLog?: AuditConnection;
+    name: string;
+    username: string;
+    email: string;
+}
+
+export interface CandidateEdge {
+    node?: Candidate;
+    cursor: Cursor;
+}
+
+export interface CandidateConnection {
+    totalCount: number;
+    pageInfo: PageInfo;
+    edges: CandidateEdge[];
+}
+
+export interface Researcher {
+    id: string;
+    auditLog?: AuditConnection;
+    createdAt: DateTime;
+    deletedAt?: DateTime;
+    verifiedAt?: DateTime;
+    name: string;
+    username: string;
+    email: string;
+}
+
+export interface ResearcherEdge {
+    node?: Trial;
+    cursor: Cursor;
+}
+
+export interface ResearcherConnection {
+    totalCount: number;
+    pageInfo: PageInfo;
+    edges: ResearcherEdge[];
+}
+
+export interface Trial {
+    id: string;
+    createdAt: DateTime;
+    deletedAt?: DateTime;
+    title: string;
+    synopsis: string;
+    description: string;
+    tags: string[];
+    startTime: DateTime;
+    endTime: DateTime;
+    frequency: Frequency;
+    auditLog?: AuditConnection;
+    organisers?: ResearcherConnection;
+    participants?: CandidateConnection;
+}
+
+export interface TrialEdge {
+    node?: Trial;
+    cursor: Cursor;
+}
+
+export interface TrialConnection {
+    totalCount: number;
+    pageInfo: PageInfo;
+    edges: TrialEdge[];
+}
+
+export interface Question {
+    id: string;
+    title: string;
+    type: QuestionType;
+    answers: Answer[];
+    trial: Trial;
+}
+
+export interface Answer {
+    id: string;
+    respondedAt: DateTime;
+    response: Response;
+}
+
+export interface ResponseEdge {
+    node?: Response;
+    cursor: Cursor;
+}
+
+export interface ResponseConnection {
+    totalCount: number;
+    pageInfo: PageInfo;
+    edges: ResponseEdge[];
+}
+
+export interface RadioResponse {
+    id: string;
+    type: QuestionType;
+    respondent: Candidate;
+    answers: Answer[];
+    response: Answer;
+}
+
+export interface CheckboxResponse {
+    id: string;
+    type: QuestionType;
+    respondent: Candidate;
+    answers: Answer[];
+    responses: Answer[];
+}
+
+export interface TextResponse {
+    id: string;
+    type: QuestionType;
+    respondent: Candidate;
+    response: string;
+}
+
+export interface ScaleResponse {
+    id: string;
+    type: QuestionType;
+    respondent: Candidate;
+    response: number;
+}
+
+export interface IQuery {
+    getTrialResponses(trialId: string): ResponseConnection | Promise<ResponseConnection>;
+}
+
+export interface IMutation {
+    createResearcherAccount(input?: ResearcherInput): Researcher | Promise<Researcher>;
+    createCandidateAccount(input?: CandidateInput): Candidate | Promise<Candidate>;
+}
+
+export type DateTime = any;
+export type Cursor = any;
+export type Actor = Candidate | Researcher;
+export type Response = RadioResponse | CheckboxResponse | ScaleResponse | TextResponse;
