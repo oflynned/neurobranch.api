b295c61b699b34bb6bbc2cf7d46829aad02e9349
Edmond Ó Floinn <oflynned@tcd.ie>
2021-04-30 17:29:35 +0200

    test: add jest configs to libs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  projects: ['<rootDir>/apps/core'],
+  projects: [
+    '<rootDir>/apps/core/src',
+    '<rootDir>/libs/common/src',
+    '<rootDir>/libs/config/src',
+    '<rootDir>/libs/entities/src',
+    '<rootDir>/libs/firebase/src',
+    '<rootDir>/libs/graphql/src',
+    '<rootDir>/libs/orm/src',
+  ],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.ts', '!**/*/index.ts'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
 };
