94e627d72a0d529c3ce8f0572ff21c15e96b52e9
Edmond Ó Floinn <oflynned@tcd.ie>
2021-04-30 17:12:07 +0200

    chore: register modules

--- apps/core/src/app/researcher/researcher.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ResearcherService } from './researcher.service';
-import { ResearcherResolver } from './researcher.resolver';
-
-@Module({
-  providers: [ResearcherService, ResearcherResolver],
-})
-export class ResearcherModule {}
