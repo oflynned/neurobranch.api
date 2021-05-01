37237388342f548e25185c82aa79c11b56ed5e99
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-01 12:03:51 +0200

    chore: tidy up app module and register dependencies

--- apps/core/src/app.module.ts
+++ apps/core/src/app.module.ts
@@ -1,14 +1,33 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { FirebaseAdminModule } from '@aginix/nestjs-firebase-admin';
 import { join } from 'path';
 
 import { PostgresModule } from '../../../libs/orm/src';
-import { CoreConfigModule, ResearcherModule } from './app';
+import { CoreConfigModule, CoreConfigService, ResearcherModule } from './app';
+import { CacheModule } from '../../../libs/cache/src';
+import { RedisModule } from 'nestjs-redis';
+import { FirebaseValidator } from '../../../libs/firebase/src';
+
+const services = [ResearcherModule];
 
 @Module({
   imports: [
     CoreConfigModule,
+    CacheModule,
     PostgresModule,
+    FirebaseAdminModule.forRootAsync({
+      useFactory: () => ({
+        credential: FirebaseValidator.applicationDefault(),
+      }),
+    }),
+    RedisModule.forRootAsync({
+      imports: [CoreConfigModule],
+      inject: [CoreConfigService],
+      useFactory: (config: CoreConfigService) => ({
+        url: config.getRedisUrl(),
+      }),
+    }),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       definitions: {
@@ -18,7 +37,7 @@ import { CoreConfigModule, ResearcherModule } from './app';
       debug: true,
       playground: true,
     }),
-    ResearcherModule,
+    ...services,
   ],
 })
 export class AppModule {}
