bcd0145126ef47e75705623027199b1afde418eb
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-01 15:36:00 +0200

    chore: renamed files

--- libs/config/src/base.config.module.ts
+++ libs/config/src/base.config.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { BaseConfigService } from './base-config.service';
+import { BaseConfigService } from './base.config.service';
 
 @Module({
   imports: [ConfigModule],
--- libs/config/src/index.ts
+++ libs/config/src/index.ts
@@ -1,2 +1,2 @@
-export * from './base-config.module';
-export * from './base-config.service';
+export * from './base.config.module';
+export * from './base.config.service';
