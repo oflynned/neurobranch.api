8011067e9feac4ac3b5cd9fb1137b86004643890
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-01 12:03:14 +0200

    chore: register firebase config before app start

--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -2,6 +2,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import * as Firebase from 'firebase-admin';
+import { RawConfigService } from './app';
+
+const firebaseConfig = RawConfigService.getInstance()
+  .ensureValues(['FIREBASE_SERVICE_ACCOUNT'])
+  .getFirebaseConfig();
+Firebase.initializeApp(firebaseConfig);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
