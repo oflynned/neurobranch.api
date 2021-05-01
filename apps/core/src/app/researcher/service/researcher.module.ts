45bea0b02f08e094cfe03bcfa5804b29dc5b6ef0
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-01 16:16:38 +0200

    chore: register auth middleware

--- apps/core/src/app/researcher/service/researcher.module.ts
+++ apps/core/src/app/researcher/service/researcher.module.ts
@@ -1,12 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ResearcherResolver } from '../graphql/researcher.resolver';
 import { ResearcherService } from './researcher.service';
 import { ResearcherRepo } from './researcher.repo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResearcherEntity } from '../../../../../../libs/entities/src';
+import { VerifyFirebaseMiddleware } from './middleware/verify-firebase.middleware';
+import { FirebaseModule } from '../../../../../../libs/firebase/src';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ResearcherEntity])],
-  providers: [ResearcherResolver, ResearcherService, ResearcherRepo],
+  imports: [TypeOrmModule.forFeature([ResearcherEntity]), FirebaseModule],
+  providers: [
+    ResearcherResolver,
+    ResearcherService,
+    ResearcherRepo,
+    VerifyFirebaseMiddleware,
+  ],
 })
-export class ResearcherModule {}
+export class ResearcherModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(VerifyFirebaseMiddleware).forRoutes('*');
+  }
+}
