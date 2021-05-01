6eda91d5482d39b4f13ec80bb48fdefa72727731
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-01 12:02:24 +0200

    feat: add raw config helper

---
+++ apps/core/src/app/config/raw.config.service.ts
@@ -0,0 +1,63 @@
+import { Optional } from '../../../../../libs/common/src';
+import { Codec } from '../../../../../libs/common/src/codec';
+import {
+  FirebaseServiceAccount,
+  CredentialStore,
+  FirebaseConfig,
+  FirebaseValidator,
+} from '../../../../../libs/firebase/src';
+
+export class RawConfigService {
+  private env: Record<string, Optional<string>>;
+  private credentialStore: CredentialStore;
+  private codec: Codec;
+
+  static getInstance(
+    env: Record<string, Optional<string>> = process.env,
+    credentialStore: CredentialStore = FirebaseValidator,
+    codec: Codec = new Codec(),
+  ): RawConfigService {
+    return new RawConfigService().withEnvironment(env, credentialStore, codec);
+  }
+
+  withEnvironment(
+    env: Record<string, Optional<string>> = {},
+    credentialStore: CredentialStore = FirebaseValidator,
+    codec: Codec = new Codec(),
+  ): RawConfigService {
+    this.env = env;
+    this.credentialStore = credentialStore;
+    this.codec = codec;
+
+    return this;
+  }
+
+  getValue(key: string, throwOnMissing = true): Optional<string> {
+    const value = this.env[key];
+    if (!value && throwOnMissing) {
+      throw new Error(`config error - missing env.${key}`);
+    }
+
+    return value;
+  }
+
+  ensureValues(keys: string[]): RawConfigService {
+    keys.forEach((k) => this.getValue(k));
+    return this;
+  }
+
+  getFirebaseConfig(): FirebaseConfig {
+    const encodedServiceAccount = this.getValue(
+      'FIREBASE_SERVICE_ACCOUNT',
+      true,
+    );
+    const decodedServiceAccount = this.codec.decode(encodedServiceAccount);
+    const serviceAccount = JSON.parse(
+      decodedServiceAccount,
+    ) as FirebaseServiceAccount;
+
+    return {
+      credential: this.credentialStore.cert(serviceAccount),
+    };
+  }
+}
