0319fbaa4961fcd4a7e5965be5cbd77f7d2f343b
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-02 15:55:09 +0200

    feat: add researcher guard

---
+++ apps/core/src/app/researcher/service/guards/researcher.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { ResearcherService } from '../researcher.service';
+
+@Injectable()
+export class ResearcherGuard implements CanActivate {
+  constructor(private readonly researcherService: ResearcherService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ctx = GqlExecutionContext.create(context).getContext();
+    const { email } = ctx.jwt;
+
+    try {
+      ctx.user = await this.researcherService.getResearcherByEmail(email);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
