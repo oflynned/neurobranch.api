1bfffb500cdc2013fa7eed364e0334031fbe1ab5
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-02 12:33:18 +0200

    chore: remove provider type

--- apps/core/src/app/researcher/dto/create-researcher.dto.ts
+++ apps/core/src/app/researcher/dto/create-researcher.dto.ts
@@ -1,9 +1,7 @@
 import { ResearcherInput } from '../../../../../../types/generated-types';
 
-export type Provider = 'GOOGLE' | 'EMAIL_PASSWORD';
-
 export type CreateResearcherDto = ResearcherInput & {
   email: string;
   providerId: string;
-  provider: Provider;
+  provider: string;
 };
