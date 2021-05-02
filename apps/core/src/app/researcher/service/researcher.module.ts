93daa4a311c4b6c788c6fd9abc663d4f1b32f38e
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-02 17:55:01 +0200

    feat: add find researcher by id

--- apps/core/src/app/researcher/service/researcher.module.ts
+++ apps/core/src/app/researcher/service/researcher.module.ts
@@ -1,15 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ResearcherResolver } from '../graphql/researcher.resolver';
 import { ResearcherService } from './researcher.service';
 import { ResearcherRepo } from './researcher.repo';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResearcherEntity } from '../../../../../../libs/entities/src';
 import { FirebaseModule } from '../../../../../../libs/firebase/src';
-import { JwtGuard } from '../graphql/guards/jwt.guard';
-import { ResearcherGuard } from '../graphql/guards/researcher.guard';
+import { JwtGuard, ResearcherGuard } from '../graphql/guards';
+import { TrialModule } from '../../trial';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ResearcherEntity]), FirebaseModule],
+  imports: [
+    TypeOrmModule.forFeature([ResearcherEntity]),
+    FirebaseModule,
+    forwardRef(() => TrialModule),
+  ],
+  exports: [ResearcherService, JwtGuard, ResearcherGuard],
   providers: [
     ResearcherResolver,
     ResearcherService,
