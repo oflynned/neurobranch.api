88b30baeb773f8ffc24abe8d11504e58683b739a
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-02 17:55:16 +0200

    feat: add trial resolver

---
+++ apps/core/src/app/trial/graphql/trial.resolver.ts
@@ -0,0 +1,52 @@
+import {
+  Args,
+  Context,
+  Mutation,
+  Parent,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../../researcher/graphql/guards';
+import {
+  CreateTrialInput,
+  Frequency,
+} from '../../../../../../types/generated-types';
+import {
+  ResearcherEntity,
+  TrialEntity,
+} from '../../../../../../libs/entities/src';
+import { ResearcherGuard } from '../../researcher/graphql/guards';
+import { TrialService } from '../service/trial.service';
+import { CreateTrialDto } from '../dto/create-trial.dto';
+
+@Resolver('Trial')
+@UseGuards(JwtGuard)
+export class TrialResolver {
+  constructor(private readonly trialService: TrialService) {}
+
+  @Mutation('createTrial')
+  @UseGuards(ResearcherGuard)
+  async createTrial(
+    @Args('input') input: CreateTrialInput,
+    @Context('user') researcher: ResearcherEntity,
+  ): Promise<TrialEntity> {
+    const timestamp = new Date();
+    const dto: CreateTrialDto = {
+      title: input.title,
+      description: 'description',
+      synopsis: 'synopsis',
+      frequency: Frequency.DAILY,
+      tags: ['tag'],
+      startTime: timestamp,
+      endTime: timestamp,
+    };
+
+    return this.trialService.createTrial(dto, researcher);
+  }
+
+  @ResolveField('lead')
+  async getTrialLead(@Parent() trial: TrialEntity): Promise<ResearcherEntity> {
+    return this.trialService.getTrialLead(trial);
+  }
+}
