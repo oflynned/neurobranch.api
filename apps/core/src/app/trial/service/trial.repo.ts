b599f0838c668e250ab44ad5e9e50f55923ef6ef
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-02 17:55:38 +0200

    feat: add trial service

---
+++ apps/core/src/app/trial/service/trial.repo.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {
+  ResearcherEntity,
+  TrialEntity,
+} from '../../../../../../libs/entities/src';
+import { CreateTrialDto } from '../dto/create-trial.dto';
+
+@Injectable()
+export class TrialRepo {
+  constructor(
+    @InjectRepository(TrialEntity)
+    private readonly repo: Repository<TrialEntity>,
+  ) {}
+
+  async createTrial(
+    dto: CreateTrialDto,
+    researcher: ResearcherEntity,
+    createdAt = new Date(),
+  ): Promise<TrialEntity> {
+    const entity = {
+      ...dto,
+      researcher,
+      createdAt,
+    };
+
+    return this.repo.save(entity);
+  }
+
+  async getTrialLead(trial: TrialEntity): Promise<ResearcherEntity> {
+    const joinedTrial = await this.repo.findOne({
+      where: { id: trial.id },
+      relations: ['researcher'],
+    });
+
+    return joinedTrial.researcher;
+  }
+
+  async getResearcherTrials(
+    researcher: ResearcherEntity,
+  ): Promise<TrialEntity[]> {
+    return this.repo.find({ researcher });
+  }
+}
