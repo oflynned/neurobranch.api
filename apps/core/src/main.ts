a8df4cf1d29e178f47df7cc4bcaea7be99bf55fe
Edmond Ó Floinn <oflynned@tcd.ie>
2021-05-24 17:38:19 +0200

    chore: enable cors

--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -12,6 +12,8 @@ Firebase.initializeApp(firebaseConfig);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const port = process.env.PORT ?? 3000;
 
   await app.listen(port, () => {
