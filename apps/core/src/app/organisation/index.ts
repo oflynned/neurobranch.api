d054bb2efa2bf39751606a811f1d7553fc4bf52d
Edmond Ó Floinn <oflynned@tcd.ie>
2021-06-06 20:31:06 +0200

    refactor: remove excess barrel export info

--- apps/core/src/app/organisation/index.ts
+++ apps/core/src/app/organisation/index.ts
@@ -1,4 +1,2 @@
 export * from './service/organisation.module';
-export * from './service/organisation.repo';
-export * from './service/organisation.resolver';
 export * from './service/organisation.service';
